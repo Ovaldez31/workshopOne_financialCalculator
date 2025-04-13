b31d1ae02e5d301e4f253708c554cd26efa8bed5
Orchid <ovaldez@appdev.year.org>
2025-04-13 10:38:05 -0400

    first commit

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -3,7 +3,20 @@ package com.ps;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        double principal = 84000.0;
+        double annualInterestRate = 0.085;
+        int loanTermYears = 10;
+
+        // Calculate monthly interest rate //
+        double monthlyInterestRate = annualInterestRate / 12.0;
+
+        // Calculate total numbers of months //
+        int numbersOfMonths = loanTermYears * 12;
+
+        // Calculate the monthly payment using the formula //
+        float monthlyPayment = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfMonths)) / (Math.pow(1 + monthlyInterestRate, numbersOfMonths) - 1);
+
+
 
 
     }

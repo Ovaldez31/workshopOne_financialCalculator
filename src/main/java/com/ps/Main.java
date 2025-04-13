00f2b223175f90ef11516730cd840dd4fe07fcd0
Orchid <ovaldez@appdev.year.org>
2025-04-13 11:27:34 -0400

    first commit

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -14,9 +14,19 @@ public class Main {
         int numbersOfMonths = loanTermYears * 12;
 
         // Calculate the monthly payment using the formula //
-        float monthlyPayment = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfMonths)) / (Math.pow(1 + monthlyInterestRate, numbersOfMonths) - 1);
+        double monthlyPayment = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numbersOfMonths)) / (Math.pow(1 + monthlyInterestRate, numbersOfMonths) - 1);
 
+        // Calculate the total amount paid //
+        double totalAmountPaid = monthlyPayment * numbersOfMonths;
 
+        // Calculate the total interest paid //
+        double totalInterestPaid = totalAmountPaid - principal;
+
+        System.out.printf("Loan Principal: $% .2f%n", principal);
+        System.out.printf("Annual Interest Rate: % .3f%%%n", annualInterestRate * 100);
+        System.out.printf("Loan Term: %d years (%d months)%n", loanTermYears, numbersOfMonths);
+        System.out.printf("Monthly Payment: $% .2f%n", monthlyPayment);
+        System.out.printf("Total Interest Paid: $% .2f%n", totalInterestPaid);
 
 
     }
